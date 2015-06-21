6b2ef9798fdd0856f8dc1a883a14ba19f7c172a4
runninglzw <459437440@qq.com>
2015-06-21 23:37:49 +0800

    记事本

---
+++ test5-6_NOtepaid/test5-6_NOtepaid/test5-6_NOtepaidDlg.h
@@ -0,0 +1,35 @@
+
+// test5-6_NOtepaidDlg.h : 头文件
+//
+
+#pragma once
+
+
+// Ctest56_NOtepaidDlg 对话框
+class Ctest56_NOtepaidDlg : public CDialogEx
+{
+// 构造
+public:
+	Ctest56_NOtepaidDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_TEST56_NOTEPAID_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+};
